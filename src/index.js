0330c13feb34a93fac92c49eefb3c0ff9977d4e0
Wes Griffin <themisguidedidiot@gmail.com>
2019-05-20 21:40:02 -0700

    created foreach loop to append list to <ul>

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import todo from '../data/todo-list-data.js';
 import template from '../src/todo-template.js';
 import htmlToDom from '../src/todo-template.js';
+
+const listElement = document.getElementById('todo-list');
+
+todo.forEach(todo => {
+    const html = template(todo);
+    const dom = htmlToDom(html);
+    listElement.appendChild(dom);
+});
